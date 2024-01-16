f393be5a70cf60a4ab1381a6ec9c3ab3ee1d0903
Ash <ashy@tyty.cloud>
2024-01-16 23:05:31 +0800

    reformatted the code so that Widgets.kt is a simplified file

--- app/src/main/java/com/example/materialpass/ElevatedCard.kt
+++ app/src/main/java/com/example/materialpass/ElevatedCard.kt
@@ -6,120 +6,26 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.ContentCopy
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
-
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
-@Composable
-fun AddPage(modifier: Modifier = Modifier) // ToDo this is for adding new information to the main page
-{
-    Column(modifier.fillMaxWidth()) {
-        TopBarAdd()
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview
-@Composable
-fun TopBarAdd(modifier: Modifier = Modifier) {
-    TopAppBar(title = { Text(text = "Add Login", modifier.padding(10.dp)) }, navigationIcon = {
-        Icon(
-            imageVector = Icons.Default.Close,
-            contentDescription = null,
-            modifier.padding(horizontal = 10.dp)
-        )
-    }, actions = {
-        Icon(
-            imageVector = Icons.Default.Done,
-            contentDescription = null,
-            modifier.padding(horizontal = 10.dp)
-        )
-    })
-}
-
-
-@Preview
-@Composable
-fun FloatingAddButton(modifier: Modifier = Modifier) {
-    FloatingActionButton(
-        onClick = { /*TODO*/ }, modifier.size(56.dp),
-        content = {
-            Icon(
-                imageVector = Icons.Default.Add, contentDescription = null
-            )
-            FabPosition.End
-        },
-    )
-    modifier.padding(16.dp)
-}
-
-
-// Data classes to store login information
-//region class information
-data class SiteInformation(
-    val title: String = "", val url: String = "N/A", val credentials: Credentials
-)
-
-data class Credentials(
-    val username: String?, val email: String?, val password: String?
-)
-
-
-var siteData = mutableListOf<SiteInformation>()
-fun addSiteData() {
-    siteData.add(
-        SiteInformation(
-            title = "Google Account", url = "https://google.com", credentials = Credentials(
-                username = "pikaprisma",
-                email = "google@tyty.cloud",
-                password = "googleStopTrackingMe"
-            )
-        )
-    )
-    siteData.add(
-        SiteInformation(
-            title = "Revolt", url = "https://revolt.chat", credentials = Credentials(
-                username = "", email = "revolt@tyty.cloud", password = "thisIsAp@ssw0rd"
-            )
-        )
-    )
-}
-//endregion
-
-
-//region Material Card
 @Composable
 fun FilledCard(
     modifier: Modifier = Modifier,
@@ -214,4 +120,3 @@ fun DisplayCard(modifier: Modifier = Modifier) {
         }
     }
 }
-
---
+++ app/src/main/java/com/example/materialpass/Widgets.kt
@@ -0,0 +1,96 @@
+package com.example.materialpass
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+//region TODO Implement this on add page
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun TopBarAdd(modifier: Modifier = Modifier) {
+    TopAppBar(title = { Text(text = "Add Login", Modifier.padding(10.dp)) }, navigationIcon = {
+        IconButton(onClick = {/* TODO */ }, content = {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = null,
+            )
+        })
+    }, actions = {
+        IconButton(onClick = { /*TODO*/ }, content = {
+            Icon(
+                imageVector = Icons.Default.Done,
+                contentDescription = null,
+            )
+        })
+    })
+}
+//endregion
+
+//region FAB TODO open add page
+@Preview
+@Composable
+fun FloatingAddButton(modifier: Modifier = Modifier) {
+    FloatingActionButton(
+        onClick = { /*TODO*/ }, modifier.size(56.dp),
+        content = {
+            Icon(
+                imageVector = Icons.Default.Add, contentDescription = null
+            )
+            FabPosition.End
+        },
+    )
+    modifier.padding(16.dp)
+}
+//endregion
+
+//region class Site Data and credentials
+// List of login data and domain/app name
+var siteData = mutableListOf<SiteInformation>()
+
+data class SiteInformation(
+    val title: String = "", val url: String = "N/A", val credentials: Credentials
+)
+
+data class Credentials(
+    val username: String?, val email: String?, val password: String?
+)
+
+fun addSiteData() {
+    siteData.add(
+        SiteInformation(
+            title = "Google Account", url = "https://google.com", credentials = Credentials(
+                username = "pikaprisma",
+                email = "google@tyty.cloud",
+                password = "googleStopTrackingMe"
+            )
+        )
+    )
+    siteData.add(
+        SiteInformation(
+            title = "Revolt", url = "https://revolt.chat", credentials = Credentials(
+                username = "", email = "revolt@tyty.cloud", password = "thisIsAp@ssw0rd"
+            )
+        )
+    )
+}
+//endregion
